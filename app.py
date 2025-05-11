31da21d2ee5ee342d9db1c50806ae188e7b4024f
mahmoudalkirm90 <mahmoudalkirm90@gmail.com>
2025-05-11 09:22:16 +0300

    json

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask
-import json
+from flask import Flask , render_template
 
 j = ['aj' , 'ab'] 
 app = Flask(__name__)
-@app.route('/j' , methods = ["GET"])
+@app.route('/')
 def homepage():
-    return json.dumps(j) 
+    return render_template('app.json')
 
 if __name__ == '__main__':
     app.run()
